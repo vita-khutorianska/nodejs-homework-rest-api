7f9f0100b484ff01365dc0b8a265446a94a88c72
Vita Khutorianska <steblvita@gmail.com>
2021-07-17 16:53:06 +0300

    delete files

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 app.use('/api/user', authRouter)
-app.use('/api', downloadRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- src/routes/api/filesDownloadRouters.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const path = require('path')
-const AVATARS_DIR = path.join('./public/avatars')
-const router = express.Router()
-router.use('/download', express.static(AVATARS_DIR))
-module.exports = router
