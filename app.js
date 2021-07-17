968d4d31733541a14e23892400b7c8cbd603af6b
Vita Khutorianska <steblvita@gmail.com>
2021-07-17 15:10:40 +0300

    add files louder to api

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-
 const contactsRouter = require('./src/routes/api/contactsRouter')
 const authRouter = require('./src/routes/api/authRouter')
-
+const downloadRouter = require('./src/routes/api/filesDownloadRouters')
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -14,6 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/contacts', contactsRouter)
 app.use('/api/user', authRouter)
+app.use('/api', downloadRouter)
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
